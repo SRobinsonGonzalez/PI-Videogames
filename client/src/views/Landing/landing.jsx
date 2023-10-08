2043e9097b19fe44825fb162d19a53453bf92dba
RobinsonG <hh.robinson95@hotmail.com>
2023-10-08 03:02:12 -0500

    Landing

--- client/src/views/Landing/landing.jsx
+++ client/src/views/Landing/landing.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import style from "./landing.module.css";
+import video from "../../assets/video/12.mp4"
 
 const Landing = () => {
     return (
         <div>
-            <h1>Landing</h1>
+            <video
+                className={style.video}
+                src={video}
+                autoPlay
+                muted
+                loop>
+            </video>
             <Link to="/home">
-                <button>Home</button>
+                <button
+                    className={style.button}>
+                    <span>HO</span><span>ME</span>
+                </button>
             </Link>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap');
+            </style>
         </div>
     )
 };
